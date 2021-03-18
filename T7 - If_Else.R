b547b6039371f187128234d92d1beff9db21f237
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-18 16:09:06 +1100

    fix indentations for my OCD

--- T7 - If_Else.R
+++ T7 - If_Else.R
@@ -22,11 +22,11 @@ if (a > b) {
 
 # if, else, elseif
 if (b > a) {
-  print("b is greater than a")
+    print("b is greater than a")
 } else if (a == b) {
-  print("a and b are equal")
+    print("a and b are equal")
 } else {
-  print("a is greater than b")
+    print("a is greater than b")
 }
 
 
@@ -37,7 +37,7 @@ b <- 33
 c <- 500
 
 if (a > b & c > a){
-  print("Both conditions are true")
+    print("Both conditions are true")
 }
 
 # The "&" can be substituted with a "&&".
@@ -54,7 +54,7 @@ if (a > b & c > a){
 
 # Or statments
 if (a > b | a > c){
-  print("At least one of the conditions is true")
+    print("At least one of the conditions is true")
 }
 
 ((-2:2) >= 0) | ((-2:2) <= 0) # Calculated using BIDMAS and left to right
--- T8 - While Loops.R
+++ T8 - While Loops.R
@@ -2,8 +2,8 @@
 
 i <- 1
 while (i < 6) { # Condition is in a bracket
-  print(i)
-  i <- i + 1
+    print(i)
+    i <- i + 1
 }
 
 
@@ -11,11 +11,11 @@ while (i < 6) { # Condition is in a bracket
 # Breaks
 i <- 1
 while (i < 6) {
-  print(i)
-  i <- i + 1
-  if (i == 4) { # Will break after three loops
-    break
-  }
+    print(i)
+    i <- i + 1
+    if (i == 4) { # Will break after three loops
+        break
+    }
 }
 
 
@@ -23,9 +23,9 @@ while (i < 6) {
 # Next
 i <- 0
 while (i < 6) {
-  i <- i + 1
-  if (i == 3) {
-    next # Restarts the loop, thus skipping 3
-  }
-  print(i)
+    i <- i + 1
+    if (i == 3) {
+        next # Restarts the loop, thus skipping 3
+    }
+    print(i)
 }
