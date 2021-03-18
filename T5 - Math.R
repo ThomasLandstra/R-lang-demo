b6bab04b2eb63736cb415eca92d96fcee5367d65
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-18 13:33:09 +1100

    Tutorial 5

---
+++ T5 - Math.R
@@ -0,0 +1,25 @@
+# Math
+
+# Basic Operators
+5 + 5
+6 - 5
+3 * 3
+12 / 4
+
+
+# Finding the max and min numbers in a set of numbers can be done like so:
+max(5, 10, 15)
+min(5, 10, 15)
+
+
+# Finding the square root can be done like so:
+sqrt(16)
+
+
+# To find the absolute value (positive) of a variable you can do abs()
+abs(-7.2) # 7.2
+
+
+# Rounding numbers
+ceiling(1.4) # Rounding up
+floor(1.4) # Rounding down
