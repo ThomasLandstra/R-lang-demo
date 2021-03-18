31602e9e9d480144caffff542f9556f193e63385
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-18 15:29:25 +1100

    Tutorial 6

---
+++ T6 - Booleans.R
@@ -0,0 +1,30 @@
+# Booleans
+## You can get booleans by using an expression commonly used in if statments.
+
+2 > 5
+7 < 9
+9 == 9
+
+a <- 10
+b <- 9
+
+a > b
+
+## Booleans are in all capitals
+x = TRUE
+y = FALSE
+
+x
+y
+
+## Comparison Operators
+x = 1
+y = 2
+
+
+x == y # Equal
+x != y # Not equal
+x > y # Greater than
+x < y # Less than
+x >= y # Greater than or equal to
+x <= y # Less than or equal to
