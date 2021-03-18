83069479799754ab56d64fce9326ccd8ac6fbf37
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-18 12:57:52 +1100

    Tutorial 1

---
+++ T1 - Basic.R
@@ -0,0 +1,7 @@
+# Basic Syntax, Comments, Graphing and Datatypes
+
+"Hello World" # Same as print("Hello World")
+print("Hello World")
+5+5 # Same as print(5+5)
+7+2
+plot(1:10) # Plots a points on a graph between 1 and 10. Points x and y value are both equal.
