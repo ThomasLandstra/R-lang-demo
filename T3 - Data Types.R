a769f6386bfe1b0dbacdb1d7251c63f6e307b36f
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-03-18 13:22:06 +1100

    renamed

---
+++ T3 - Data Types.R
@@ -0,0 +1,25 @@
+# Data Types
+
+my_var <- 30 # my_var is type of numeric
+my_var <- "Sally" # my_var is now of type character (aka string)
+
+# numeric
+x <- 10.5 # print(x): prints 10.5
+class(x)
+
+# integer
+x <- 1000L # print(x): prints 1000
+class(x)
+
+# complex
+x <- 9i + 3 # print(x): prints 3+9i
+class(x)
+
+# character/string
+x <- "R is exciting" # print(x) prints: "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE # print(x) prints: True
+x
+class(x)
